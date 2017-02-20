976a5838240ab0cf4f8345f97ad8076b42e6c9fc
Kathy Joshi <kathyjoshi@yahoo.com>
2017-02-20 22:08:52 +0000

    second attempt

--- create_box/main.py
+++ create_box/main.py
@@ -1,15 +1,19 @@
-"""This is the entry point of the program."""
-
-
 def create_box(height, width, character):
+    box =""
+    line = character * width + "\n"
     for i in range (height):        # This defines the number of rows
-       print (character * width)   # print char passed multiplied by width passed 
+       box = box + line
+    return box   #  char passed multiplied by width passed 
             
         
 ##create_box (3,4,'*')
-##create_box (5,2,'@')   
+##create_box (5,2,'@') 
+##create_box(3,24,'x')
+
+
+
             
 
 
 if __name__ == '__main__':
-    create_box(3, 4, '*')
+  create_box(3, 4, '*')
