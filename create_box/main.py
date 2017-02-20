63cec5ffe54c01b40e025fc9d9396043cb21317f
Kathy Joshi <kathyjoshi@yahoo.com>
2017-02-20 18:53:25 +0000

    first attempt at make box

--- create_box/main.py
+++ create_box/main.py
@@ -2,7 +2,13 @@
 
 
 def create_box(height, width, character):
-    pass
+    for i in range (height):        # This defines the number of rows
+       print (character * width)   # print char passed multiplied by width passed 
+            
+        
+##create_box (3,4,'*')
+##create_box (5,2,'@')   
+            
 
 
 if __name__ == '__main__':
